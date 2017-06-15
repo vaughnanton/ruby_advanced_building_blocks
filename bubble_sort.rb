294cac09cb7b0a7438a65d4db68eacae9fcb3ae5
Vaughn Antonyan <vaughnanton@gmail.com>
2017-06-14 17:10:03 -0700

    bubble_sort final

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,20 @@
-#initialize bubblesort
+def bubble_sort(array)
+  #while array is not equal to its sorted self
+  while array != array.sort
+    #from index 0 up to length minus 2
+    0.upto(array.length-2) do |i|
+      #compare indices
+      if array[i] > array[i+1]
+        array[i],array[i+1] = array[i+1],array[i]
+      end
+    end
+  end
+  print array
+end
+
+num = [7, 3, 6, 1, 2, 5, 4]
+num1 = [7, 5, 3, 6, 8]
+num2 = [3, 2, 1]
+bubble_sort(num)
+bubble_sort(num1)
+bubble_sort(num2)
