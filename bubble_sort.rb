a0ec03c5a9bce82d10ca4b7cf2334c7aa80140b5
Vaughn Antonyan <vaughnanton@gmail.com>
2017-06-14 18:42:06 -0700

    Bubblesort complete

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,3 +18,19 @@ num2 = [3, 2, 1]
 bubble_sort(num)
 bubble_sort(num1)
 bubble_sort(num2)
+
+def bubble_sort_by(array)
+  #while array is not equal to its sorted self
+  while array != array.sort
+    #from index 0 up to length minus 2
+    0.upto(array.length-2) do |i|
+      #compare indices
+      if array[i] > array[i+1]
+        array[i],array[i+1] = array[i+1],array[i]
+      end
+    end
+  end
+  print array
+end
+
+bubble_sort_by(["hi","hello","hey"]) {|left,right| right.length - left.length}
