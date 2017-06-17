860cedc45a2890df7c8fcc7456ad3b67f99e5ce6
Vaughn Antonyan <vaughnanton@gmail.com>
2017-06-16 21:08:19 -0700

    Code skeleton initialize

--- enumerable.rb
+++ enumerable.rb
@@ -1,1 +1,21 @@
-#initialize enumerable
+module Enumerable
+  def my_each
+  end
+  def my_each_with_index
+  end
+  def my_select
+    use my_each here but not each
+  end
+  def my_all
+  end
+  def my_any?
+  end
+  def my_none?
+  end
+  def my_count
+  end
+  def my_map
+  end
+  def my_inject
+  end
+end
