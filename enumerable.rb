a87e7cd49afeab0a29c60ac85dc4bf8067e3e00b
Vaughn Antonyan <vaughnanton@gmail.com>
2017-06-19 14:07:54 -0700

    Update methods

--- enumerable.rb
+++ enumerable.rb
@@ -1,11 +1,27 @@
 module Enumerable
+
   def my_each
+    i = 0
+    while i < self.size
+        yield(self[i])
+        i += 1
+    end
+    self
   end
+
   def my_each_with_index
+    i = 0
+    while i < self.size
+      yield(self[i], i)
+      i += 1
+    end
+    self
   end
+
   def my_select
     use my_each here but not each
   end
+  
   def my_all
   end
   def my_any?
