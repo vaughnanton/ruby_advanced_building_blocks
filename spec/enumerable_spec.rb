0df3d39ffc7b768a5d9b0c8d52a7b6c307bd36a0
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-23 18:23:59 -0700

    spec complete

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,1 +1,22 @@
 require './spec_helper'
+
+describe Enumerable do
+  include enumerable
+
+  let(:base_array) {[1,4,3,6,7,8]}
+  
+  describe "#{}my_each" do
+
+    context "when no block is given" do
+      it "returns an enumerable object" do
+        expect(base_array.my_each)to be_instance_of(Enumerator)
+      end
+    end
+
+    context "when a block is given" do
+      it "returns an array object" do
+        expect(base_array.my_each {|num| num}).to be_instance_of(Array)
+      end
+    end
+  end
+end
